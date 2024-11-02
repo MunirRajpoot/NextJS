5eb39ad779bae2965b172749f68985a775611f34
MunirRajpoot <munirrajpoot1012@gmail.com>
2024-11-02 08:47:26 -0700

    second commit

--- src/app/api/employee/route.js
+++
@@ -1,7 +0,0 @@
-import { employee } from "@/db/db";
-import { NextResponse } from "next/server";
-
-export const GET=(request)=>{
-    let data=employee;
-    return NextResponse.json(data,{status:200})
-}
--- src/app/contact/layout.js
+++ src/app/contact/layout.js
@@ -1,19 +1,19 @@
 'use client'
 import { usePathname } from "next/navigation"
 
-export default function Layout({children}){
-   const currentPath= usePathname();
-   console.log(currentPath);
-   
-return (
-<>
-{
-    currentPath!=='/contact/employee' ?  <h2>Common Layouts</h2> : null
-}
-   
-    {
-        children
-    }
-    </>
-)
+export default function Layout({ children }) {
+    const currentPath = usePathname();
+    console.log(currentPath);
+
+    return (
+        <>
+            {
+                currentPath !== '/contact/employee' ? <h2>Common Layouts</h2> : null
+            }
+
+            {
+                children
+            }
+        </>
+    )
 }
